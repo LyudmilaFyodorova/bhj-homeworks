6760baf5aa575be5bd1ef6c3b3bba8b1a291be61
Lyudmila <lyudmilabravo@gmail.com>
2025-07-21 20:06:43 +0700

    исправление 3 задания

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,28 +1,28 @@
 const allHoles = document.querySelectorAll('.hole');
-const arr = Array.from(allHoles);
 const deadStatus = document.getElementById('dead');
 const lostStatus = document.getElementById('lost');
 let deadStart = 0;
 let lostStart = 0;
 
-arr.forEach(item =>
-    arr[i].onclick = function () {
-        for (let i = 0; i < arr.length; i++) {
-            if (hole.className.includes('hole_has-mole')) {
-                deadStart++
-            } else {
-                lostStart++
-            }
-            
-            deadStatus.textContent = deadStart;
-            lostStatus.textContent = lostStart;
-
-            if (deadStatus === 10) {
-                alert('Вы победили!')
-            }
-            if (lostStatus === 5) {
-                alert('Вы проиграли!')
-            }
-        }
+allHoles.forEach(item => item.onclick = function () {
+    if (item.className.includes('hole_has-mole')) {
+        deadStart++
+    } else {
+        lostStart++
     }
+
+    deadStatus.textContent = deadStart;
+    lostStatus.textContent = lostStart;
+
+    if (deadStart === 10) {
+        alert('Вы победили!')
+        deadStart = 0;
+        lostStart = 0;
+    }
+    if (lostStart === 5) {
+        alert('Вы проиграли!')
+        deadStart = 0;
+        lostStart = 0;
+    }
+}
 )
