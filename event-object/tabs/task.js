41982ef6b002839a0c567c2e240d85e52c44f9ae
Lyudmila <lyudmilabravo@gmail.com>
2025-08-05 21:28:55 +0700

    задание с вкладками

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,14 +3,13 @@ const tabContents = document.querySelectorAll('.tab__content');
 const tabArray = Array.from(tabs);
 const tabContentsArray = Array.from(tabContents);
 
-tabs.forEach(item => item.addEventListener('click', function () {
+tabs.forEach((item, index) => item.addEventListener('click', function () {
     let activeTabPosition = tabArray.findIndex((item) => item.className.includes('tab_active'));
     let activeTabContent = tabContentsArray.findIndex((item) => item.className.includes('tab__content_active'));
 
     tabArray[activeTabPosition].classList.remove("tab_active");
     tabContentsArray[activeTabContent].classList.remove("tab__content_active");
 
-    tabArray[activeTabPosition].classList.add("tab_active");
-    tabContentsArray[activeTabPosition].classList.add("tab__content_active");
+    tabArray[index].classList.add("tab_active");
+    tabContentsArray[index].classList.add("tab__content_active");
 }))
- 
